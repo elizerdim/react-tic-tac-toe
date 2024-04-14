14ef73fe298992c364ac8a7bc29cafb05be54e38
Eliz Erdim <elizerdim@gmail.com>
2024-04-14 14:31:15 +0300

    Create Game component

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,19 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-export default function Board() {
-  const [ xIsNext, setXIsNext ] = useState(true);
-  const [ squares, setSquares ] = useState(Array(9).fill(null));
-
+function Board({ squares, xIsNext, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
-    const nextSign = xIsNext ? 'X' : 'O';
-    nextSquares[i] = nextSign;
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = `Winner: ${winner}`;
+    status = winner === 'tie' ? `It's a tie!` : `Winner: ${winner}`;
   } else {
     status = `Next Player: ${xIsNext ? 'X' : 'O'}`
   }
@@ -49,6 +45,32 @@ export default function Board() {
   );  
 }
 
+export default function Game() {
+  const [ xIsNext, setXIsNext ] = useState(true);
+  const [ history, setHistory ] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {    
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board 
+          squares={currentSquares}
+          xIsNext={xIsNext}
+          onPlay={handlePlay}
+        />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  )
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -64,6 +86,8 @@ function calculateWinner(squares) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
+    } else if (!squares.includes(null)) {
+      return 'tie';
     }
   }
   return null;
